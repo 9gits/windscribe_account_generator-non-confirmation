829c10f945d3dc33dae496a323a1588ec9172a29
9gits <99726976+9gits@users.noreply.github.com>
2022-02-15 19:35:17 +0900

    test version

---
+++ non-confirmation.py
@@ -0,0 +1,83 @@
+from re import search
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+path = "./chromedriver.exe"
+url = "https://windscribe.com/signup"
+filepath = "./account.txt"
+
+
+class Account:
+    def __init__(self):
+        self.name = ''
+        self.passowrd = ''
+
+def main():
+    #open browser
+    driver = webdriver.Chrome(executable_path=path)
+
+    #open URL
+    driver.get(url)
+
+    #wait
+    time.sleep(3)
+
+    #click_random_name
+    random_nane_btn = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/form/div[2]/a[1]')
+    random_nane_btn.click()
+
+    #wait
+    time.sleep(3)
+
+    #click_random_password
+    random_password_btn = driver.find_element_by_xpath('/html/body/div[1]/div[3]/div/div/form/div[2]/a[2]')
+    random_password_btn.click()
+
+    #wait
+    time.sleep(3)
+
+
+    #get_account_information
+    name_value = driver.find_element_by_xpath('//*[@id="username"]')
+    Account.name = name_value.get_attribute('value')
+
+    password_value = driver.find_element_by_xpath('//*[@id="pass1"]')
+    Account.password = password_value.get_attribute('value')
+
+
+    #click_create_account
+    create_account_btn = driver.find_element_by_xpath('//*[@id="signup_button"]')
+    create_account_btn.click()
+
+    #wait
+    time.sleep(3)
+
+    #signup-anyway (by no-confirmation)
+    signup_anyway_btn = driver.find_element_by_xpath('/html/body/div[1]/div[4]/div/ul/li[1]/a')
+    signup_anyway_btn.click()
+
+    #wait
+    time.sleep(10)
+
+    #close browser
+    driver.close()
+
+    #output information
+    print(Account.name)
+    print(Account.password)
+
+    #write account information to txtfile
+    write_account_information()
+    
+    
+def write_account_information():
+    #open file mode:'a'
+    with open(filepath,'a') as file:
+        print(Account.name,file=file)
+        print(Account.password,file=file)
+        print('\n',file=file)
+
+
+if __name__ == '__main__':
+    main()
